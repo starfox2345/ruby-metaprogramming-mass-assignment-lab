f9efcfa1b918e74c21c8bf5231332f08ef4b07f7
yatsun <yatsunlam25@gmail.com>
2021-03-24 18:59:09 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
 class Person
   #your code here
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width 
+
+  # @@all = []
+
+  # def initialize(name:, hair_color:, birthday:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
+  #   @name = name
+  #   @hair_color = hair_color
+  #   @birthday = birthday
+  #   @eye_color = eye_color
+  #   @height = height
+  #   @weight = weight
+  #   @handed = handed
+  #   @complexion =complexion
+  #   @t_shirt_size = t_shirt_size
+  #   @wrist_size = wrist_size
+  #   @glove_size = glove_size
+  #   @pant_length = pant_length
+  #   @pant_width = pant_width
+  # end
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
 end
